cd27474a6d48d1e6bc394f0cc49534b97478acc5
Blake Hu <64202537+blake-hu@users.noreply.github.com>
2024-12-07 16:31:05 -0600

    Add walking sound

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -1,56 +1,67 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
   /// <summary>
-  /// The camera attached to the player
-  /// </summary>
-  private Transform CameraTransform;
-
-  /// <summary>
-  /// Fields for firing bullets
+  ///     Fields for firing bullets
   /// </summary>
   public GameObject BulletPrefab;
-  public float FireInterval;
-  public float BulletVelocity = 100f;
-  private float nextFire;
 
-  // Start is called before the first frame update
-  void Start()
-  {
-    nextFire = 0f;
-    CameraTransform = transform.GetChild(0).transform;
-  }
+    public float FireInterval;
+    public float BulletVelocity = 100f;
 
-  void Update()
-  {
-    MaybeFire();
-  }
+    private AudioSource _audioSource;
+    private Rigidbody _playerRb;
 
-  /// <summary>
-  /// Fire if player is pushing button
-  /// and cooldown is over
-  /// </summary>
-  void MaybeFire()
-  {
-    var fire = Input.GetButtonDown("Fire1");
-    if (fire && Time.time > nextFire)
+    /// <summary>
+    ///     The camera attached to the player
+    /// </summary>
+    private Transform CameraTransform;
+
+    private float nextFire;
+
+    // Start is called before the first frame update
+    private void Start()
     {
-      FireBullet();
-      nextFire = Time.time + FireInterval;
+        nextFire = 0f;
+        CameraTransform = transform.GetChild(0).transform;
+        _playerRb = GetComponent<Rigidbody>();
+        _audioSource = GetComponent<AudioSource>();
     }
-  }
 
-  void FireBullet()
-  {
-    var verticalOffset = new Vector3(0, 0.5f, 0);
-    var bullet = Instantiate(BulletPrefab, transform.position + verticalOffset, Quaternion.identity);
-    bullet.transform.rotation = CameraTransform.rotation;
-    bullet.GetComponent<Rigidbody>().velocity = BulletVelocity * bullet.transform.forward;
-  }
+    private void Update()
+    {
+        MaybeFire();
+        PlayWalkingSound();
+    }
 
+    private void PlayWalkingSound()
+    {
+        if (_playerRb.velocity != Vector3.zero && !_audioSource.isPlaying)
+            _audioSource.Play();
+        else if (_playerRb.velocity == Vector3.zero && _audioSource.isPlaying)
+            _audioSource.Stop();
+    }
+
+    /// <summary>
+    ///     Fire if player is pushing button
+    ///     and cooldown is over
+    /// </summary>
+    private void MaybeFire()
+    {
+        var fire = Input.GetButtonDown("Fire1");
+        if (fire && Time.time > nextFire)
+        {
+            FireBullet();
+            nextFire = Time.time + FireInterval;
+        }
+    }
+
+    private void FireBullet()
+    {
+        var verticalOffset = new Vector3(0, 0.5f, 0);
+        var bullet = Instantiate(BulletPrefab, transform.position + verticalOffset, Quaternion.identity);
+        bullet.transform.rotation = CameraTransform.rotation;
+        bullet.GetComponent<Rigidbody>().velocity = BulletVelocity * bullet.transform.forward;
+    }
 }
-
-
